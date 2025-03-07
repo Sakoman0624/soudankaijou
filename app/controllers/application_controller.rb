c83a659607451a1bc55569f130db8d06d41ee1d3
EC2 Default User <ec2-user@ip-172-31-36-217.ap-northeast-1.compute.internal>
2025-03-07 02:53:53 +0000

    ログイン設定修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_authentication
   before_action :check_guest_user, only: [:create, :update]
- 
+
   private
-  
+
   def admin_controller?
     self.class.module_parent_name == 'Admin'
   end
-  
+
   def check_guest_user
     if user_signed_in? && current_user.guest?
       redirect_to request.referer, alert: 'ゲストユーザーはこの操作を実行できません。'
     end
   end
- 
+
   def configure_authentication
     if admin_controller?
       authenticate_admin!
@@ -21,8 +21,8 @@ class ApplicationController < ActionController::Base
       authenticate_user! unless action_is_public?
     end
   end
- 
+
   def action_is_public?
-    controller_name == 'homes' && action_name == 'top'
+    controller_name == 'homes' && %w[top about].include?(action_name)
   end
 end
