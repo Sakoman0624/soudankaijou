e5daf65f380dc2f642864f2a06bd170a772e33f1
EC2 Default User <ec2-user@ip-172-31-36-217.ap-northeast-1.compute.internal>
2025-04-27 22:45:48 +0000

    検索の修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,8 +4,8 @@ class Public::SearchesController < ApplicationController
   def search
     @range = params[:range]
     
-    if @range == "User"
-      @users = User.looks(params[:search], params[:word]).where(public: true).order(created_at: :desc).page(params[:page]).per(20)
+    if @range == "作成者名"
+      @rooms = Room.joins(:user).merge(User.looks(params[:search], params[:word])).where(public: true).order(created_at: :desc).page(params[:page]).per(12)
     else
       @rooms = Room.looks(params[:search], params[:word]).where(public: true).order(created_at: :desc).page(params[:page]).per(12)
     end
